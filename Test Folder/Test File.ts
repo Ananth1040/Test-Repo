049c0a5694e59738f8d988a995eb36508f5bffc3
Ananth1040 <104429844+Ananth1040@users.noreply.github.com>
2022-05-13 16:43:53 +0530

    JSON Format
    Test File

---
+++ Test Folder/Test File.ts
@@ -0,0 +1,260 @@
+{
+	"Postal_code_of_delivery": "G0M 1P0",
+	"Owner": {
+		"name": "Marie-Eve Bolduc",
+		"id": "449573000000136016",
+		"email": "marieeve@publicitemaca.ca"
+	},
+	"Email": "souziel@hotmail.com",
+	"Profil_client": {
+		"name": "STRUCTURE SBL INC.",
+		"id": "449573000066170001"
+	},
+	"$currency_symbol": "CA$",
+	"$field_states": null,
+	"Customer_delivery_date": "2022-06-24",
+	"Country_of_delivery": "Québec",
+	"$followers": null,
+	"$sharing_permission": "full_access",
+	"Name": "768-C25017",
+	"Last_Activity_Time": "2022-05-13T06:41:08-04:00",
+	"Order_managed_by": "Martine ext 272",
+	"Type_de_commande": null,
+	"livraison_vers": null,
+	"$state": "save",
+	"Unsubscribed_Mode": null,
+	"$process_flow": false,
+	"Stratege": {
+		"name": "Marie-Eve Bolduc",
+		"id": "449573000000136016"
+	},
+	"Verified_invoice": false,
+	"Exchange_Rate": 1,
+	"Customer_s_carrier": null,
+	"Currency": "CAD",
+	"Pass_to_the_Maca_warehouse": false,
+	"Total_transport": 0,
+	"Purchase_order_number": "768-C25017",
+	"id": "449573000120043057",
+	"$approved": true,
+	"Status": "En attente de confirmation",
+	"$approval": {
+		"delegate": false,
+		"approve": false,
+		"reject": false,
+		"resubmit": false
+	},
+	"$followed": false,
+	"$wizard_connection_path": null,
+	"$editable": true,
+	"Courriel_Facturation": "slecours@structuresbl.com",
+	"Attention_of": "MACA - 25017",
+	"Purchased_items": [],
+	"Date_suivi_B_O": null,
+	"Purchase_Order_No": "SO18642",
+	"Internal_note_not_visible": "Répétition de la commande 22102.\nOn commande plus d'étiquettes que la commande, revient moins cher.",
+	"COMMANDE_Client": {
+		"name": "Casquettes avec filet",
+		"id": "449573000118881001"
+	},
+	"Fixed_date_event": false,
+	"Date_of_purchase": "2022-05-12",
+	"Supplier_Name1": "Suzanne Lapointe",
+	"City_of_delivery": "St-Benoît-Labre",
+	"Total_Amount": 1500,
+	"Calculate_Total": false,
+	"$review_process": {
+		"approve": false,
+		"reject": false,
+		"resubmit": false
+	},
+	"Handling_to_dos": null,
+	"Date_du_suivi_a_faire": "2022-05-16",
+	"$canvas_id": null,
+	"$review": null,
+	"Prochaine_action": "Transmettre la commande chez Suzanne et ensuite Suivi de confirmation",
+	"Delivery_tracking_no_or_url": null,
+	"Order_Number": "C25017",
+	"Supplier": {
+		"name": "Suzanne Lapointe",
+		"id": "449573000063343353"
+	},
+	"External_note_visible": null,
+	"Extras_Item": [{
+		"Description": "Item : Casquette avec filet \nDescription :",
+		"Total_Amount": 1500,
+		"$in_merge": false,
+		"$field_states": null,
+		"Created_Time": "2022-05-12T16:10:27-04:00",
+		"Parent_Id": {
+			"name": "768-C25017",
+			"id": "449573000120043057"
+		},
+		"Product": {
+			"name": "Étiquette [É43143]",
+			"id": "449573000117643143"
+		},
+		"Quantity": 600,
+		"id": "449573000120043058",
+		"Unit_Price": 2.5,
+		"Serial_No": "1"
+	}, {
+		"Description": null,
+		"Total_Amount": 0,
+		"$in_merge": false,
+		"$field_states": null,
+		"Created_Time": "2022-05-12T16:10:27-04:00",
+		"Parent_Id": {
+			"name": "768-C25017",
+			"id": "449573000120043057"
+		},
+		"Product": {
+			"name": "Frais de livraison a déterminer [F06025]",
+			"id": "449573000118906025"
+		},
+		"Quantity": 1,
+		"id": "449573000120043059",
+		"Unit_Price": 0,
+		"Serial_No": "1"
+	}],
+	"Language_Format": "French/English",
+	"Contact_Livraison": "Hugo Veilleux",
+	"Tracking_URL": null,
+	"Unsubscribed_Time": null,
+	"Date_d_exp_dition_du_fournisseur": "2022-06-16",
+	"$orchestration": false,
+	"Delivery_street": "35B, rue Industrielle",
+	"$in_merge": false,
+	"Tag": [],
+	"$approval_state": "approved"
+}
+Tess {
+	"Postal_code_of_delivery": "G0M 1P0",
+	"Owner": {
+		"name": "Marie-Eve Bolduc",
+		"id": "449573000000136016",
+		"email": "marieeve@publicitemaca.ca"
+	},
+	"Email": "souziel@hotmail.com",
+	"Profil_client": {
+		"name": "STRUCTURE SBL INC.",
+		"id": "449573000066170001"
+	},
+	"$currency_symbol": "CA$",
+	"$field_states": null,
+	"Customer_delivery_date": "2022-06-24",
+	"Country_of_delivery": "Québec",
+	"$followers": null,
+	"$sharing_permission": "full_access",
+	"Name": "768-C25017",
+	"Last_Activity_Time": "2022-05-13T06:41:08-04:00",
+	"Order_managed_by": "Martine ext 272",
+	"Type_de_commande": null,
+	"livraison_vers": null,
+	"$state": "save",
+	"Unsubscribed_Mode": null,
+	"$process_flow": false,
+	"Stratege": {
+		"name": "Marie-Eve Bolduc",
+		"id": "449573000000136016"
+	},
+	"Verified_invoice": false,
+	"Exchange_Rate": 1,
+	"Customer_s_carrier": null,
+	"Currency": "CAD",
+	"Pass_to_the_Maca_warehouse": false,
+	"Total_transport": 0,
+	"Purchase_order_number": "768-C25017",
+	"id": "449573000120043057",
+	"$approved": true,
+	"Status": "En attente de confirmation",
+	"$approval": {
+		"delegate": false,
+		"approve": false,
+		"reject": false,
+		"resubmit": false
+	},
+	"$followed": false,
+	"$wizard_connection_path": null,
+	"$editable": true,
+	"Courriel_Facturation": "slecours@structuresbl.com",
+	"Attention_of": "MACA - 25017",
+	"Purchased_items": [],
+	"Date_suivi_B_O": null,
+	"Purchase_Order_No": "SO18642",
+	"Internal_note_not_visible": "Répétition de la commande 22102.\nOn commande plus d'étiquettes que la commande, revient moins cher.",
+	"COMMANDE_Client": {
+		"name": "Casquettes avec filet",
+		"id": "449573000118881001"
+	},
+	"Fixed_date_event": false,
+	"Date_of_purchase": "2022-05-12",
+	"Supplier_Name1": "Suzanne Lapointe",
+	"City_of_delivery": "St-Benoît-Labre",
+	"Total_Amount": 1500,
+	"Calculate_Total": false,
+	"$review_process": {
+		"approve": false,
+		"reject": false,
+		"resubmit": false
+	},
+	"Handling_to_dos": null,
+	"Date_du_suivi_a_faire": "2022-05-16",
+	"$canvas_id": null,
+	"$review": null,
+	"Prochaine_action": "Transmettre la commande chez Suzanne et ensuite Suivi de confirmation",
+	"Delivery_tracking_no_or_url": null,
+	"Order_Number": "C25017",
+	"Supplier": {
+		"name": "Suzanne Lapointe",
+		"id": "449573000063343353"
+	},
+	"External_note_visible": null,
+	"Extras_Item": [{
+		"Description": "Item : Casquette avec filet \nDescription :",
+		"Total_Amount": 1500,
+		"$in_merge": false,
+		"$field_states": null,
+		"Created_Time": "2022-05-12T16:10:27-04:00",
+		"Parent_Id": {
+			"name": "768-C25017",
+			"id": "449573000120043057"
+		},
+		"Product": {
+			"name": "Étiquette [É43143]",
+			"id": "449573000117643143"
+		},
+		"Quantity": 600,
+		"id": "449573000120043058",
+		"Unit_Price": 2.5,
+		"Serial_No": "1"
+	}, {
+		"Description": null,
+		"Total_Amount": 0,
+		"$in_merge": false,
+		"$field_states": null,
+		"Created_Time": "2022-05-12T16:10:27-04:00",
+		"Parent_Id": {
+			"name": "768-C25017",
+			"id": "449573000120043057"
+		},
+		"Product": {
+			"name": "Frais de livraison a déterminer [F06025]",
+			"id": "449573000118906025"
+		},
+		"Quantity": 1,
+		"id": "449573000120043059",
+		"Unit_Price": 0,
+		"Serial_No": "1"
+	}],
+	"Language_Format": "French/English",
+	"Contact_Livraison": "Hugo Veilleux",
+	"Tracking_URL": null,
+	"Unsubscribed_Time": null,
+	"Date_d_exp_dition_du_fournisseur": "2022-06-16",
+	"$orchestration": false,
+	"Delivery_street": "35B, rue Industrielle",
+	"$in_merge": false,
+	"Tag": [],
+	"$approval_state": "approved"
+}
